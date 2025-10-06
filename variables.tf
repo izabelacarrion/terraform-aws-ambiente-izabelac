d1ca2f4f2e3d32063bbe6bbe1aebdeb1056a7265
izabelacarrion <izabelacarrioncastro@gmail.com>
2025-10-06 11:14:51 -0300

    fix: não precisa ser obrigatório essa variável

--- variables.tf
+++ variables.tf
@@ -6,9 +6,11 @@ variable "nome_instancia" {
 variable "environment_instancia" {
   type        = string
   description = "Ambiente da instancia"
+  default     = "Staging"
 }
 
 variable "ebs_block_devices" {
   description = "Lista de volumes EBS para ser criado e montado na instancia"
   type        = list(any) #Lista de qualquer coisa
+  default     = []
 }
